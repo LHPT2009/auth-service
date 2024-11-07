20e087fa11ac6cdf3ad9f18e0be26e9cf21dbd22
Tung <lehuynhphuongtung0601@gmail.com>
2024-11-07 23:48:15 +0700

    develop

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,12 +1,15 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "123",
-    database: "manager_app",
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT || '5432'),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     synchronize: false,
     entities: [`${__dirname}/../src/**/entity/*.{ts,js}`],
     migrations: [`${__dirname}/../migrations/data/*.{ts,js}`],
